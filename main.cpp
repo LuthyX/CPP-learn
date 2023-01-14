1e0052e4c856e89981b586da4be5a2c494803873
LuthyX <peteradegoke81@gmail.com>
2023-01-14 22:52:38 +0100

    basics

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+// BASIC DATA TYPES
+
+    // char grade = 'A';
+    // string phrase = "Tonight is gonna be Legendary";
+    // bool isGood = true;
+    // int age = 50;
+    // double price = 50.49;
+
+    // cout << phrase.substr(3,2);
+
+    // double pri = 5.98;
+    
+    // int age;
+    // string name;
+    
+    // cout<< "Whhat is yyyour age?" << endl;
+    // cin>>age;
+
+    // cout<< "You are " << age << " years old" <<endl;
+
+    // getline(cin, name);
+
+    // cout<< name;
+
+    // int num1, num2;
+
+    // cout<< "Enter num 1: ";
+    // cin>> num1;
+    // cout << "Enter num 2: ";
+    // cin >> num2;
+
+    // int num = num1 + num2;
+    // cout<<num<<endl;
+
+    string celeb;
+    // cout<< "enter a color : ";
+    // cin>>color;
+    // cout<< "enter a noun: ";
+    // cin>> noun;
+
+    cout<<"enter a celeb: ";
+    getline(cin, celeb);
+
+    // cout<< "Roses are " << color<< endl;
+    // cout<< noun <<" are blue" << endl;
+    cout<< "I love " << celeb << endl;
+    
+
+    return 0;
+
+}
